757d34ac6e47857a6c9af5c781126ed3bd4f7974
rvcas <x@rvcas.dev>
2022-06-17 18:51:20 -0400

    feat: more pretty printing logic

--- crates/uplc/src/pretty.rs
+++ crates/uplc/src/pretty.rs
@@ -6,7 +6,7 @@ impl Program<Name> {
     pub fn to_pretty(&self) -> String {
         let mut w = Vec::new();
 
-        self.to_doc().render(20, &mut w).unwrap();
+        self.to_doc().render(100, &mut w).unwrap();
 
         String::from_utf8(w).unwrap()
     }
@@ -17,7 +17,7 @@ impl Program<Name> {
         RcDoc::text("(")
             .append(RcDoc::text("program"))
             .append(RcDoc::line())
-            .append(RcDoc::text(version))
+            .append(RcDoc::text(version).nest(2))
             .append(RcDoc::line())
             .append(self.term.to_doc())
             .append(RcDoc::line_())
@@ -30,31 +30,35 @@ impl Term<Name> {
         match self {
             Term::Var(name) => RcDoc::text(format!("{}_{}", name.text, name.unique)),
             Term::Delay(term) => RcDoc::text("(")
-                .append(RcDoc::line_())
-                .append(RcDoc::text("delay"))
-                .append(RcDoc::line())
-                .append(term.to_doc())
-                .append(RcDoc::line_())
+                .append(
+                    RcDoc::text("delay")
+                        .append(RcDoc::line())
+                        .append(term.to_doc())
+                        .append(RcDoc::line_()),
+                )
                 .append(RcDoc::text(")")),
             Term::Lambda {
                 parameter_name,
                 body,
             } => RcDoc::text("(")
-                .append(RcDoc::line_())
-                .append(RcDoc::text(format!(
-                    "{}_{}",
-                    parameter_name.text, parameter_name.unique
-                )))
-                .append(RcDoc::line())
-                .append(body.to_doc())
+                .append(
+                    RcDoc::text("lam")
+                        .append(RcDoc::line())
+                        .append(RcDoc::text(format!(
+                            "{}_{}",
+                            parameter_name.text, parameter_name.unique
+                        )))
+                        .append(RcDoc::line())
+                        .append(body.to_doc()),
+                )
                 .append(RcDoc::line_())
                 .append(RcDoc::text(")")),
             Term::Apply { function, argument } => RcDoc::text("[")
-                .append(RcDoc::line_())
+                .append(RcDoc::line())
                 .append(function.to_doc())
                 .append(RcDoc::line())
                 .append(argument.to_doc())
-                .append(RcDoc::line_())
+                .append(RcDoc::line())
                 .append(RcDoc::text("]")),
             Term::Constant(constant) => RcDoc::text("(")
                 .append(RcDoc::text("con"))
@@ -62,25 +66,22 @@ impl Term<Name> {
                 .append(constant.to_doc())
                 .append(RcDoc::text(")")),
             Term::Force(term) => RcDoc::text("(")
-                .append(RcDoc::line_())
                 .append(RcDoc::text("force"))
                 .append(RcDoc::line())
                 .append(term.to_doc())
                 .append(RcDoc::line_())
                 .append(RcDoc::text(")")),
             Term::Error => RcDoc::text("(")
-                .append(RcDoc::line_())
                 .append(RcDoc::text("error"))
-                .append(RcDoc::line_())
                 .append(RcDoc::text(")")),
             Term::Builtin(builtin) => RcDoc::text("(")
-                .append(RcDoc::line_())
                 .append(RcDoc::text("builtin"))
                 .append(RcDoc::line())
                 .append(RcDoc::text(builtin.to_string()))
                 .append(RcDoc::line_())
                 .append(RcDoc::text(")")),
         }
+        .nest(2)
     }
 }
 
