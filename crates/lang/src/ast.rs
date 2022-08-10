208f2e80eae5128e154b0863f526f039e0fd7d0c
rvcas <x@rvcas.dev>
2022-08-10 15:50:19 -0400

    feat: more ast elements and some sample syntax

--- crates/lang/src/ast.rs
+++ crates/lang/src/ast.rs
@@ -1,5 +1,47 @@
-pub struct Module {}
+pub struct Module {
+    pub name: Vec<String>,
+    pub docs: Vec<String>,
+    pub is_script: bool,
+    pub is_lib: bool,
+    pub is_policy: bool,
+}
 
-pub enum Decl {}
+pub enum Definition {
+    Fn {
+        arguments: Vec<String>,
+        body: Expr,
+        doc: Option<String>,
+        name: String,
+        public: bool,
+        return_annotation: Option<()>,
+        return_type: (),
+    },
+
+    TypeAlias {
+        alias: String,
+        annotation: (),
+        doc: Option<String>,
+        parameters: Vec<String>,
+        public: bool,
+        tipo: (),
+    },
+
+    DataType {
+        constructors: Vec<()>,
+        doc: Option<String>,
+        name: String,
+        opaque: bool,
+        parameters: Vec<String>,
+        public: bool,
+        typed_parameters: Vec<()>,
+    },
+
+    Use {
+        module: Vec<String>,
+        as_name: Option<String>,
+        // unqualified: Vec<UnqualifiedImport>,
+        // package: PackageName,
+    },
+}
 
 pub enum Expr {}
