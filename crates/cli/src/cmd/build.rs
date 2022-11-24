67d160230b5adb2b73a1522ef6ff8e761d77f131
rvcas <x@rvcas.dev>
2022-11-24 18:14:32 -0500

    feat: new build command flag
      - `uplc` to optionally dump raw uplc

--- crates/cli/src/cmd/build.rs
+++ crates/cli/src/cmd/build.rs
@@ -6,8 +6,12 @@ pub struct Args {
     /// Path to project
     #[clap(short, long)]
     directory: Option<PathBuf>,
+
+    /// Also dump textual uplc
+    #[clap(short, long)]
+    uplc: bool,
 }
 
-pub fn exec(Args { directory }: Args) -> miette::Result<()> {
-    crate::with_project(directory, |p| p.build())
+pub fn exec(Args { directory, uplc }: Args) -> miette::Result<()> {
+    crate::with_project(directory, |p| p.build(uplc))
 }
