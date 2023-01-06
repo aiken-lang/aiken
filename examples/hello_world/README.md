193792b48fa04024a2ee973b815399d741f17b4d
KtorZ <matthias.benkort@gmail.com>
2023-01-06 13:44:20 +0100

    Draft quick README, use ENV var for passing configuration.

---
+++ examples/hello_world/README.md
@@ -0,0 +1,33 @@
+# Hello, World!
+
+An example of an Hello, World! contract using Aiken and [Lucid](https://github.com/spacebudz/lucid).
+
+See the [full tutorial on aiken-lang.org](https://aiken-lang.org/getting-started/hello-world).
+
+## Building
+
+```
+aiken build
+```
+
+## Generating Credentials
+
+```
+deno run --allow-net --allow-write generate-credentials.ts
+```
+
+## Locking Funds
+
+> **Warning** Require `BLOCKFROST_API_KEY` environment variable to be set.
+
+```
+deno run --allow-net --allow-read hello_world-lock.ts
+```
+
+## Unlocking Funds
+
+> **Warning** Require `BLOCKFROST_API_KEY` environment variable to be set.
+
+```
+deno run --allow-net --allow-read hello_world-unlock.ts -- TRANSACTION_ID_FROM_LOCK
+```
--- examples/hello_world/hello_world-lock.ts
+++ examples/hello_world/hello_world-lock.ts
@@ -13,7 +13,7 @@ import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
 const lucid = await Lucid.new(
   new Blockfrost(
     "https://cardano-preview.blockfrost.io/api/v0",
-     /* BLOCKFROST API KEY */
+    Deno.env.get('BLOCKFROST_API_KEY'),
   ),
   "Preview",
 );
@@ -21,9 +21,9 @@ const lucid = await Lucid.new(
 lucid.selectWalletFromPrivateKey(await Deno.readTextFile("./key.sk"));
 
 const validator = await readValidator("./assets/hello_world/spend/script.cbor");
- 
+
 // --- Supporting functions
- 
+
 async function readValidator(filepath: String): Promise<SpendingValidator> {
   return {
     type: "PlutusV2",
@@ -39,27 +39,27 @@ const publicKeyHash = lucid.utils
 const datum = Data.to(new Constr(0, [ publicKeyHash ]));
 
 const txLock = await lock(1000000, { into: validator, owner: datum });
- 
+
 await lucid.awaitTx(txLock);
- 
+
 console.log(`1 ADA locked into the contract
     Tx ID: ${txLock}
     Datum: ${datum}
 `);
- 
+
 // --- Supporting functions
 
 async function lock(lovelace, { into, owner }): Promise<TxHash>{
   const contractAddress = lucid.utils.validatorToAddress(into);
- 
+
   const tx = await lucid
     .newTx()
     .payToContract(contractAddress, { inline: owner }, { lovelace })
     .complete();
- 
+
   const signedTx = await tx
     .sign()
     .complete();
- 
+
   return signedTx.submit();
 }
