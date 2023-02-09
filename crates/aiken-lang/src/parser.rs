e9caa710c46b3c3547c1cab610ee049ed3281e08
rvcas <x@rvcas.dev>
2023-02-09 01:05:11 -0500

    chore: rename assert_parser to expect_parser

--- crates/aiken-lang/src/parser.rs
+++ crates/aiken-lang/src/parser.rs
@@ -1028,7 +1028,7 @@ pub fn expr_parser(
                 },
             );
 
-        let assert_parser = just(Token::Expect)
+        let expect_parser = just(Token::Expect)
             .ignore_then(pattern_parser())
             .then(just(Token::Colon).ignore_then(type_parser()).or_not())
             .then_ignore(just(Token::Equal))
@@ -1093,7 +1093,7 @@ pub fn expr_parser(
             block_parser,
             when_parser,
             let_parser,
-            assert_parser,
+            expect_parser,
             if_parser,
         ));
 
