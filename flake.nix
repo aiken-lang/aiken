29d76a3d89b02b49a99c66a952147f63ac109860
Micah Kendall <micahk.me@icloud.com>
2023-04-26 18:39:21 +0800

    bump cargo2nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 #
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
     flake-utils.follows = "cargo2nix/flake-utils";
     nixpkgs.follows = "cargo2nix/nixpkgs";
     devshell.url = "github:numtide/devshell";
