c910e0054e9146d0e32c730f0b53ba182270b8cd
rvcas <x@rvcas.dev>
2023-11-07 16:02:20 -0500

    test(bls): constant parsing tests

--- crates/aiken-lang/src/parser/definition/constant.rs
+++ crates/aiken-lang/src/parser/definition/constant.rs
@@ -84,3 +84,28 @@ pub fn value() -> impl Parser<Token, ast::Constant, Error = ParseError> {
         constant_bytearray_parser,
     ))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_definition;
+
+    #[test]
+    fn g1_element_constant() {
+        assert_definition!(
+            r#"
+            pub const point =
+              #<Bls12_381, G1>"950dfd33da2682260c76038dfb8bad6e84ae9d599a3c151815945ac1e6ef6b1027cd917f3907479d20d636ce437a41f5"
+            "#
+        );
+    }
+
+    #[test]
+    fn g2_element_constant() {
+        assert_definition!(
+            r#"
+            pub const point =
+              #<Bls12_381, G2>"b0629fa1158c2d23a10413fe91d381a84d25e31d041cd0377d25828498fd02011b35893938ced97535395e4815201e67108bcd4665e0db25d602d76fa791fab706c54abf5e1a9e44b4ac1e6badf3d2ac0328f5e30be341677c8bac5dda7682f1"
+            "#
+        );
+    }
+}
