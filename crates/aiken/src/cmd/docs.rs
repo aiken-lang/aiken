40c0fa7d77f8940b4dbb6f1c0f7013b647961baa
KtorZ <matthias.benkort@gmail.com>
2023-11-25 14:52:51 +0100

    Add --watch flag to the 'build' and 'docs' commands too.

--- crates/aiken/src/cmd/docs.rs
+++ crates/aiken/src/cmd/docs.rs
@@ -1,4 +1,4 @@
-use aiken_project::watch::with_project;
+use aiken_project::watch::{self, watch_project, with_project};
 use std::path::PathBuf;
 
 #[derive(clap::Args)]
@@ -11,6 +11,10 @@ pub struct Args {
     #[clap(short = 'D', long)]
     deny: bool,
 
+    /// When enabled, re-run the command on file changes instead of exiting
+    #[clap(short, long)]
+    watch: bool,
+
     /// Output directory for the documentation
     #[clap(short = 'o', long)]
     destination: Option<PathBuf>,
@@ -20,8 +24,15 @@ pub fn exec(
     Args {
         directory,
         deny,
+        watch,
         destination,
     }: Args,
 ) -> miette::Result<()> {
-    with_project(directory.as_deref(), deny, |p| p.docs(destination.clone()))
+    if watch {
+        watch_project(directory.as_deref(), watch::default_filter, 500, |p| {
+            p.docs(destination.clone())
+        })
+    } else {
+        with_project(directory.as_deref(), deny, |p| p.docs(destination.clone()))
+    }
 }
