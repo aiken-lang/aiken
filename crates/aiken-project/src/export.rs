196bccc4eb21139b733c8935a81d9ea0af11cad6
rvcas <x@rvcas.dev>
2024-04-04 17:03:22 -0400

    test: add some snapshot tests for export type

--- crates/aiken-project/src/export.rs
+++ crates/aiken-project/src/export.rs
@@ -38,11 +38,6 @@ impl Export {
         generator: &mut CodeGenerator,
         modules: &CheckedModules,
     ) -> Result<Export, blueprint::Error> {
-        let program = generator
-            .generate_raw(&func.body, &func.arguments, &module.name)
-            .to_debruijn()
-            .unwrap();
-
         let mut definitions = Definitions::new();
 
         let parameters = func
@@ -69,6 +64,11 @@ impl Export {
             })
             .collect::<Result<_, _>>()?;
 
+        let program = generator
+            .generate_raw(&func.body, &func.arguments, &module.name)
+            .to_debruijn()
+            .unwrap();
+
         Ok(Export {
             name: format!("{}.{}", &module.name, &func.name),
             doc: func.doc.clone(),
@@ -76,5 +76,108 @@ impl Export {
             program,
             definitions,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{CheckedModules, Export};
+    use crate::tests::TestProject;
+    use aiken_lang::{
+        self,
+        ast::{TraceLevel, Tracing},
+    };
+
+    macro_rules! assert_export {
+        ($code:expr) => {
+            let mut project = TestProject::new();
+
+            let modules = CheckedModules::singleton(project.check(project.parse(indoc::indoc! { $code })));
+
+            let mut generator = project.new_generator(
+                Tracing::All(TraceLevel::Verbose),
+            );
+
+            let (module, func) = modules
+                .functions()
+                .next()
+                .expect("source code did no yield any exports");
+
+            let export = Export::from_function(func, module, &mut generator, &modules);
+
+            match export {
+                Err(e) => insta::with_settings!({
+                    description => concat!("Code:\n\n", indoc::indoc! { $code }),
+                    omit_expression => true
+                }, {
+                    insta::assert_debug_snapshot!(e);
+                }),
+
+                Ok(validator) => insta::with_settings!({
+                    description => concat!("Code:\n\n", indoc::indoc! { $code }),
+                    omit_expression => true
+                }, {
+                    insta::assert_json_snapshot!(validator);
+                }),
+            };
+        };
+    }
+
+    #[test]
+    fn basic_export() {
+        assert_export!(
+            r#"
+            pub fn add(a: Int, b: Int) -> Int {
+                a + b
+            }
+            "#
+        );
+    }
+
+    #[test]
+    fn illegal_opaque_type() {
+        assert_export!(
+            r#"
+            pub opaque type Thing {
+              a: Int
+            }
+
+            pub fn add(a: Thing, b: Int) -> Int {
+                a.a + b
+            }
+            "#
+        );
+    }
+
+    #[test]
+    fn recursive_types() {
+        assert_export!(
+            r#"
+            pub type Foo<a> {
+              Empty
+              Bar(a, Foo<a>)
+            }
+
+            pub fn add(a: Foo<Int>, b: Foo<Int>) -> Int {
+              when (a, b) is {
+                (Empty, Empty) -> 0
+                (Bar(x, y), Bar(c, d)) -> x + c + add(y, d)
+                (Empty, Bar(c, d)) -> c + add(Empty, d)
+                (Bar(x, y), Empty) -> x + add(y, Empty)
+              }
+            }
+            "#
+        );
+    }
+
+    #[test]
+    fn cannot_export_generics() {
+        assert_export!(
+            r#"
+            pub fn add(_a: a, _b: b) -> Bool {
+                True
+            }
+            "#
+        );
     }
 }
