fed464278d89359ca1618c62795b4d087873ec5c
KtorZ <matthias.benkort@gmail.com>
2024-08-31 00:01:46 +0200

    Allow field access on 'else'.

--- crates/aiken-lang/src/parser/chain/field_access.rs
+++ crates/aiken-lang/src/parser/chain/field_access.rs
@@ -1,16 +1,16 @@
-use chumsky::prelude::*;
-
 use super::Chain;
 use crate::{
     expr::UntypedExpr,
     parser::{token::Token, ParseError},
 };
+use chumsky::prelude::*;
 
 pub(crate) fn parser() -> impl Parser<Token, Chain, Error = ParseError> {
     just(Token::Dot)
-        .ignore_then(select! {
-            Token::Name { name } => name,
-        })
+        .ignore_then(choice((
+            select! { Token::Else => "else".to_string() },
+            select! { Token::Name { name } => name, },
+        )))
         .map_with_span(Chain::FieldAccess)
 }
 
@@ -27,4 +27,19 @@ pub(crate) fn constructor() -> impl Parser<Token, UntypedExpr, Error = ParseErro
                 name: module,
             }),
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_expr;
+
+    #[test]
+    fn field_access_else_1() {
+        assert_expr!("foo.else");
+    }
+
+    #[test]
+    fn field_access_else_2() {
+        assert_expr!("foo.bar.else");
+    }
 }
