26eedb90d460c4e97dc21422e002f2946ffb744a
Riley-Kilgore <Riley_Kilgore@outlook.com>
2024-10-22 09:48:26 -0700

    Added target awareness and fixed property based test json output

--- crates/aiken/src/cmd/check.rs
+++ crates/aiken/src/cmd/check.rs
@@ -5,6 +5,7 @@ use aiken_lang::{
 };
 use aiken_project::watch::{self, watch_project, with_project};
 use rand::prelude::*;
+use std::io::{self, IsTerminal};
 use std::{path::PathBuf, process};
 
 #[derive(clap::Args)]
@@ -84,10 +85,6 @@ pub struct Args {
     /// [optional]
     #[clap(short, long, value_parser=trace_level_parser(), default_value_t=TraceLevel::Verbose, verbatim_doc_comment)]
     trace_level: TraceLevel,
-
-    /// Output JSON (useful for scripting & automation)
-    #[clap(long)]
-    json: bool,
 }
 
 pub fn exec(
@@ -104,12 +101,13 @@ pub fn exec(
         seed,
         max_success,
         env,
-        json,
     }: Args,
 ) -> miette::Result<()> {
     let mut rng = rand::thread_rng();
 
     let seed = seed.unwrap_or_else(|| rng.gen());
+
+    let json_output = !io::stdout().is_terminal();
 
     let result = if watch {
         watch_project(directory.as_deref(), watch::default_filter, 500, |p| {
@@ -125,26 +123,31 @@ pub fn exec(
                     None => Tracing::All(trace_level),
                 },
                 env.clone(),
-                json,
+                json_output,
             )
         })
     } else {
-        with_project(directory.as_deref(), deny, json, |p| {
-            p.check(
-                skip_tests,
-                match_tests.clone(),
-                debug,
-                exact_match,
-                seed,
-                max_success,
-                match filter_traces {
-                    Some(filter_traces) => filter_traces(trace_level),
-                    None => Tracing::All(trace_level),
-                },
-                env.clone(),
-                json,
-            )
-        })
+        with_project(
+            directory.as_deref(),
+            deny,
+            !io::stdout().is_terminal(),
+            |p| {
+                p.check(
+                    skip_tests,
+                    match_tests.clone(),
+                    debug,
+                    exact_match,
+                    seed,
+                    max_success,
+                    match filter_traces {
+                        Some(filter_traces) => filter_traces(trace_level),
+                        None => Tracing::All(trace_level),
+                    },
+                    env.clone(),
+                    json_output,
+                )
+            },
+        )
     };
 
     result.map_err(|_| process::exit(1))
