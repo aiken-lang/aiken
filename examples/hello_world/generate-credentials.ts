86f598488b9ad70362df031b3bfa364dccbbe78e
Jingles <jinglescode@gmail.com>
2024-12-25 16:09:16 +0800

    update code and include package.json

--- examples/hello_world/generate-credentials.ts
+++ examples/hello_world/generate-credentials.ts
@@ -1,16 +1,20 @@
-import { MeshWallet } from '@meshsdk/core';
-import fs from 'node:fs';
- 
-const secret_key = MeshWallet.brew(true) as string;
- 
-fs.writeFileSync('me.sk', secret_key);
- 
-const wallet = new MeshWallet({
-  networkId: 0,
-  key: {
-    type: 'root',
-    bech32: secret_key,
-  },
-});
- 
-fs.writeFileSync('me.addr', wallet.getUnusedAddresses()[0]);
+import { MeshWallet } from "@meshsdk/core";
+import fs from "node:fs";
+
+async function main() {
+  const secret_key = MeshWallet.brew(true) as string;
+
+  fs.writeFileSync("me.sk", secret_key);
+
+  const wallet = new MeshWallet({
+    networkId: 0,
+    key: {
+      type: "root",
+      bech32: secret_key,
+    },
+  });
+
+  fs.writeFileSync("me.addr", (await wallet.getUnusedAddresses())[0]);
+}
+
+main();
